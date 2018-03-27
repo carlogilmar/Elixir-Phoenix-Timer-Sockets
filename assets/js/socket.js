32d1842f2d99f1efeb623b41e640165012b20cdc
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-27 00:25:57 -0600

    Agregando channel en el socket js

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -59,4 +59,8 @@ channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
+channel.on("new_time", msg => {
+  console.log("The timer is ", msg.time)
+})
+
 export default socket
